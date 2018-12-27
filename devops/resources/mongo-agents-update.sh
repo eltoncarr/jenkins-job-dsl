75deda928c665ea1deb04cfb64b6763d5c66b988
syed-awais-ali <awaisalisabir@gmail.com>
2018-12-27 17:21:39 +0500

    test

--- devops/resources/mongo-agents-update.sh
+++ devops/resources/mongo-agents-update.sh
@@ -4,5 +4,5 @@ set -x
 
 cd $WORKSPACE/configuration/playbooks
 
-ansible-playbook -i ./ec2.py --limit tag_Name_edx-admin-mms mongo_mms.yml -e@../../configuration-internal/ansible/vars/edx.yml -e@../../configurationc-secure/ansible/vars/edx.yml -u ubuntu
+ansible-playbook -i ./ec2.py --limit tag_Name_edx-admin-mms mongo_mms.yml -e@../../configuration-internal/ansible/vars/edx.yml -e@../../configuration-secure/ansible/vars/edx.yml -u ubuntu
 
